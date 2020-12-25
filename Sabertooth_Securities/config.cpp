c3297c5c79dcba5d3e2e6ea98cb36ab3bfeea60e
Barais20 <52481962+Barais20@users.noreply.github.com>
2020-12-24 19:50:14 -0500

    Update config.cpp

---
+++ Sabertooth_Securities/config.cpp
@@ -0,0 +1,62 @@
+class cfgPatches
+{
+	class Sabertooth_Securities
+	{
+		author="Thomas & Krevan";
+		name="STS Mod";
+		fileName="Sabertooth_Securities.pbo";
+		units[]=
+		{
+
+		};
+		magazines[]={};
+		ammo[]={};
+		requiredVersion=0.1;
+		requiredAddons[]=
+		{
+			"3DEN",
+			"V_FZ_Armor",
+			"OPTRE_FC_Core",
+			"OPTRE_Core",
+			"V_SO_Core",
+			"V_SO_Magazines",
+			"V_SO_Weapons",
+			"A3_Weapons_F",
+			"A3_Weapons_F_Launchers_RPG32",
+			"A3_Sounds_F",
+			"A3_Sounds_F_Arsenal",
+			"A3_Weapons_F_EPA",
+			"A3Data",
+			"A3_Data_F",
+			"OPTRE_Weapons",
+			"OPTRE_Weapons_Shotgun",
+			"OPTRE_Weapons_Rockets",
+			"OPTRE_Weapons_Charges",
+			"OPTRE_Weapons_AR",
+			"A3_Data_F_Mark",
+			"OPTRE_Weapons_SMG",
+			"OPTRE_Weapons_Pistol",
+			"OPAEX_Aircore",
+			"OPAEX_Core"
+		};
+		weapons[]={};
+	};
+};
+class CfgEditorCategories
+{
+	class Sabertooth
+	{
+		displayName="Sabertooth Security";
+	};
+};
+class CfgEditorSubcategories
+{
+	class Sabertooth_Vehicles
+	{
+		displayName="Vehicles";
+	};
+};
+class CfgVehicles
+{
+  
+};
