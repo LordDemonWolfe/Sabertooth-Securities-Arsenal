a57b8b40d2e3fc5ffa5ca7be420cc5df511c7bb8
Barais20 <52481962+Barais20@users.noreply.github.com>
2021-02-12 18:46:17 -0500

    Update config.cpp
    Added thrust scripts back in since they were apparently deleted?

--- Sabertooth_Securities_Vehicles/config.cpp
+++ Sabertooth_Securities_Vehicles/config.cpp
@@ -432,6 +432,69 @@ class cfgPatches
 				tf_hasLRradio=1;
 				tf_isolatedAmount=0.40000001;
 				tf_range=50000;
+				class UserActions
+				{
+					class Thruster400Engage
+					{
+						animPeriod=5;
+						condition="(!(this getvariable [""OPTRE_Thruster_EngagedStatus"",false])) AND (player == driver this) AND (alive this) AND (isEngineOn this) AND  ((getPosATL this) select 2) > 1";
+						displayName="<t color='#04B45F'>Engage Forward Thrusters";
+						displayNameDefault="<t color='#04B45F'>Engage Forward Thrusters";
+						onlyForPlayer=0;
+						position="cargo_door_handle";
+						priority=10;
+						radius=100000;
+						showWindow=0;
+						statement="0 = this spawn V_FZ_fnc_Thruster400Engage";
+						textToolTip="<t color='#04B45F'>Engage Forward Thrusters";
+						userActionID=52;
+					};
+					class Thruster400Disengage
+					{
+						animPeriod=5;
+						condition="(this getvariable [""OPTRE_Thruster_EngagedStatus"",false]) AND (player == driver this) AND (alive this)";
+						displayName="<t color='#FCE205'>Disengage Forward Thrusters";
+						displayNameDefault="<t color='#FCE205'>Disengage Forward Thrusters";
+						onlyForPlayer=0;
+						position="cargo_door_handle";
+						priority=10;
+						radius=100000;
+						showWindow=0;
+						statement="0 = this spawn V_FZ_fnc_Thruster400Disengage";
+						textToolTip="<t color='#FCE205'>Disengage Forward Thrusters";
+						userActionID=53;
+					};
+					class FullAirbrakeEngageFast
+					{
+						animPeriod=5;
+						condition="(player == driver this) AND (alive this) AND ((speed this) > 100)";
+						displayName="<t color='#FE2E2E'>Engage Airbrakes";
+						displayNameDefault="<t color='#FE2E2E'>Engage Airbrakes";
+						onlyForPlayer=0;
+						position="cargo_door_handle";
+						priority=10;
+						radius=100000;
+						showWindow=0;
+						statement="0 = this spawn V_FZ_fnc_FullAirbrakeEngageFast; 0 = this spawn V_FZ_fnc_ThrusterDeAnimate";
+						textToolTip="<t color='#FE2E2E'>Engage Airbrakes";
+						userActionID=57;
+					};
+					class HalfAirbrakeEngageFast
+					{
+						animPeriod=5;
+						condition="(player == driver this) AND (alive this) AND ((speed this) > 450)";
+						displayName="<t color='#FE2E2E'>Engage Airbrakes (Half)";
+						displayNameDefault="<t color='#F28D00'>Engage Airbrakes (Half)";
+						onlyForPlayer=0;
+						position="cargo_door_handle";
+						priority=10;
+						radius=100000;
+						showWindow=0;
+						statement="0 = this spawn V_FZ_fnc_HalfAirbrakeEngageFast; 0 = this spawn V_FZ_fnc_ThrusterDeAnimate";
+						textToolTip="<t color='#FE2E2E'>Engage Airbrakes (Half)";
+						userActionID=58;
+					};
+				};
 				class Components: Components
 				{
 					class TransportPylonsComponent
@@ -923,7 +986,6 @@ class cfgPatches
 					""
 				};
 			};
-
 			class Sabertooth_Falcon: VES_UH144_A
 			{
 				dlc="Sabertooth Securities";
@@ -938,6 +1000,70 @@ class cfgPatches
 				tf_hasLRradio=1;
 				tf_isolatedAmount=0.40000001;
 				tf_range=50000;
+				hiddenSelections[] = { "camo1","camo2","camo3","camo4","clan","clan_text","insignia" };
+				class UserActions
+				{
+					class Thruster400Engage
+					{
+						animPeriod=5;
+						condition="(!(this getvariable [""OPTRE_Thruster_EngagedStatus"",false])) AND (player == driver this) AND (alive this) AND (isEngineOn this) AND  ((getPosATL this) select 2) > 1";
+						displayName="<t color='#04B45F'>Engage Forward Thrusters";
+						displayNameDefault="<t color='#04B45F'>Engage Forward Thrusters";
+						onlyForPlayer=0;
+						position="cargo_door_handle";
+						priority=10;
+						radius=100000;
+						showWindow=0;
+						statement="0 = this spawn V_FZ_fnc_Thruster400Engage";
+						textToolTip="<t color='#04B45F'>Engage Forward Thrusters";
+						userActionID=52;
+					};
+					class Thruster400Disengage
+					{
+						animPeriod=5;
+						condition="(this getvariable [""OPTRE_Thruster_EngagedStatus"",false]) AND (player == driver this) AND (alive this)";
+						displayName="<t color='#FCE205'>Disengage Forward Thrusters";
+						displayNameDefault="<t color='#FCE205'>Disengage Forward Thrusters";
+						onlyForPlayer=0;
+						position="cargo_door_handle";
+						priority=10;
+						radius=100000;
+						showWindow=0;
+						statement="0 = this spawn V_FZ_fnc_Thruster400Disengage";
+						textToolTip="<t color='#FCE205'>Disengage Forward Thrusters";
+						userActionID=53;
+					};
+					class FullAirbrakeEngageFast
+					{
+						animPeriod=5;
+						condition="(player == driver this) AND (alive this) AND ((speed this) > 100)";
+						displayName="<t color='#FE2E2E'>Engage Airbrakes";
+						displayNameDefault="<t color='#FE2E2E'>Engage Airbrakes";
+						onlyForPlayer=0;
+						position="cargo_door_handle";
+						priority=10;
+						radius=100000;
+						showWindow=0;
+						statement="0 = this spawn V_FZ_fnc_FullAirbrakeEngageFast; 0 = this spawn V_FZ_fnc_ThrusterDeAnimate";
+						textToolTip="<t color='#FE2E2E'>Engage Airbrakes";
+						userActionID=57;
+					};
+					class HalfAirbrakeEngageFast
+					{
+						animPeriod=5;
+						condition="(player == driver this) AND (alive this) AND ((speed this) > 450)";
+						displayName="<t color='#FE2E2E'>Engage Airbrakes (Half)";
+						displayNameDefault="<t color='#F28D00'>Engage Airbrakes (Half)";
+						onlyForPlayer=0;
+						position="cargo_door_handle";
+						priority=10;
+						radius=100000;
+						showWindow=0;
+						statement="0 = this spawn V_FZ_fnc_HalfAirbrakeEngageFast; 0 = this spawn V_FZ_fnc_ThrusterDeAnimate";
+						textToolTip="<t color='#FE2E2E'>Engage Airbrakes (Half)";
+						userActionID=58;
+					};
+				};
 				class TextureSources
 				{
 					class tex_nml
